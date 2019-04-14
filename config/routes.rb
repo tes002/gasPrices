d8561893a7e0919245461d2e055164518f109b7a
tes002 <teresa51891@gmail.com>
2019-04-13 22:54:31 -0700

    Added mode using scaffoldl: user, gast_sation; added seeds file; serialization. Implemented view function.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  resources :gas_stations
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+User.create({
+	name: "user1",
+        email:"tt@gmail.com",
+        latitude: 32.87125,
+        longitude: -117.23339
+})
+
+GasStation.create({
+	name: "Mobil",
+	address: "3233 La Jolla Villagr Dr, La Jolla, CA 92037",
+	latitude: 32.87125,
+	longitude: -117.23339,
+	price: 4.1
+})
