56aebbb1cc3a240d1f74a5627f17037216b0c4d8
tes002 <teresa51891@gmail.com>
2019-04-14 22:44:19 -0700

    Cheapest nearby function implemented. TODO: more tests for corner case.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -38,6 +38,54 @@ class UsersController < ApplicationController
     @user.destroy
   end
 
+  # GET /users/1/nearby
+  def nearbydefault
+
+  end
+
+  # PUT /users/1/nearby
+  def nearby
+    @user = User.find(params[:id])
+    @gas_stations = GasStation.all
+    if @gas_stations.present?
+      if @user.update(user_params)
+        latitude = @user.latitude
+        longitude = @user.longitude
+      else
+	render json: {status: 'ERROR', message:'Obtained no geolocation of user', data:gas_station.errors}, status: :unprocessable_entity
+	return
+      end
+      @gas_station = nil
+      price = nil
+      # initial radius to find gas station: 11km (6.9miles), 
+      lat_radius = 0.1
+      lng_radius = 0.01
+      calculated_set = Set.new()
+      while lat_radius < 2 && lng_radius < 0.2 && calculated_set.size() < GasStation.count
+	for @station in @gas_stations
+	  if (@user.latitude - @station.latitude).abs < lat_radius && (@user.longitude - @station.longitude).abs < lng_radius && !calculated_set.include?(@station)
+	    calculated_set.add(@station)
+	    if @gas_station == nil || @station.price < price
+	      @gas_station = @station
+	      price = @gas_station.price
+	    end
+	  end
+	end
+	if @gas_station == nil
+	  lat_radius += 0.1
+	  lng_radius += 0.01
+	else 
+	  render json: {status: 'SUCCESS', message:'Found cheapest nearby gas station', data:@gas_station},status: :ok
+	  return
+	end
+      end
+      render json: {status: 'SUCCESS', message:'No gas station nearby'},status: :ok
+      return
+    else
+      render json: {status: 'SUCCESS', message:'No gas station recorded'},status: :ok
+    end
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   resources :gas_stations
   resources :users
+  put 'users/:id/nearby', to: 'users#nearby'
+  get 'users/:id/nearby', to: 'users#nearbydefault'
+  # match 'users/:id/nearby', to: 'users#nearby', via: :put
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
